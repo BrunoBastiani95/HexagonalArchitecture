55ac4ab55545e12779a222c7ee1e689d6b94ac05
BrunoBastiani95 <brunobastiani95@gmail.com>
2022-11-03 19:45:30 -0300

    add: added dbcontext, entities, status machine and tests

---
+++ HotelBooking/BookingService/Core/Domain/Entities/Booking.cs
@@ -0,0 +1,32 @@
+﻿using Domain.Enums;
+
+namespace Domain.Entities
+{
+    public class Booking
+    {
+        public Booking()
+        {
+            Status = EnStatus.Created;
+        }
+
+        public int Id { get; set; }
+        public DateTime PlacedAt { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        private EnStatus Status { get; set; }
+        public EnStatus CurrentStatus { get { return Status; } }
+    
+        public void ChangeState(EnAction action)
+        {
+            Status = (Status, action) switch
+            {
+                (EnStatus.Created, EnAction.Pay) => EnStatus.Paid,
+                (EnStatus.Created, EnAction.Cancel) => EnStatus.Canceled,
+                (EnStatus.Paid, EnAction.Finish) => EnStatus.Finished,
+                (EnStatus.Paid, EnAction.Refound) => EnStatus.Refounded,
+                (EnStatus.Canceled, EnAction.Reopen) => EnStatus.Created,
+                _ => Status
+            };
+        }
+    }
+}
---
+++ HotelBooking/BookingService/Tests/Domain/DomainTests/BookingTest/StateMachineTests.cs
@@ -0,0 +1,97 @@
+﻿using Domain.Entities;
+using Domain.Enums;
+using NUnit.Framework;
+
+namespace DomainTests.BookingTest
+{
+    public class StateMachineTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void ShouldAlwaysStartWithCreatedStatus()
+        {
+            var booking = new Booking();
+
+            Assert.That(booking.CurrentStatus, Is.EqualTo(EnStatus.Created));
+        }
+
+        [Test]
+        public void ShouldSetStatusToPaidWhenPayingForABookingWithCreatedStatus()
+        {
+            var booking = new Booking();
+
+            booking.ChangeState(EnAction.Pay);
+
+            Assert.That(booking.CurrentStatus, Is.EqualTo(EnStatus.Paid));
+        }
+
+        [Test]
+        public void ShouldSetStatusToCanceledWhenCancelingABookingWithCreatedStatus()
+        {
+            var booking = new Booking();
+
+            booking.ChangeState(EnAction.Cancel);
+
+            Assert.That(booking.CurrentStatus, Is.EqualTo(EnStatus.Canceled));
+        }
+
+        [Test]
+        public void ShouldSetStatusToFinishedWhenFinishingAPaidBooking()
+        {
+            var booking = new Booking();
+
+            booking.ChangeState(EnAction.Pay);
+            booking.ChangeState(EnAction.Finish);
+
+            Assert.That(booking.CurrentStatus, Is.EqualTo(EnStatus.Finished));
+        }
+
+        [Test]
+        public void ShouldSetStatusToRefoundWhenRefoundingAPaidBooking()
+        {
+            var booking = new Booking();
+
+            booking.ChangeState(EnAction.Pay);
+            booking.ChangeState(EnAction.Refound);
+
+            Assert.That(booking.CurrentStatus, Is.EqualTo(EnStatus.Refounded));
+        }
+
+        [Test]
+        public void ShouldSetStatusToCreatedWhenReopeningACanceledBooking()
+        {
+            var booking = new Booking();
+
+            booking.ChangeState(EnAction.Cancel);
+            booking.ChangeState(EnAction.Reopen);
+
+            Assert.That(booking.CurrentStatus, Is.EqualTo(EnStatus.Created));
+        }
+
+        [Test]
+        public void ShouldNotChangeStatusWhenRefoundingABookingWithCreatedStatus()
+        {
+            var booking = new Booking();
+
+            booking.ChangeState(EnAction.Refound);
+
+            Assert.That(booking.CurrentStatus, Is.EqualTo(EnStatus.Created));
+        }
+
+        [Test]
+        public void ShouldNotChangeStatusWhenRefoundingAFinishedBooking()
+        {
+            var booking = new Booking();
+
+            booking.ChangeState(EnAction.Pay);
+            booking.ChangeState(EnAction.Finish);
+            booking.ChangeState(EnAction.Refound);
+
+            Assert.That(booking.CurrentStatus, Is.EqualTo(EnStatus.Finished));
+        }
+    }
+}
